adde403814cfa0a4870c5de56cb3fc3c879421c2
Jim DeVona <jim.devona@gmail.com>
2012-04-13 08:30:56 -0400

    Now including tentative map links. disabled observer info

--- WheresThatSat.rb
+++ WheresThatSat.rb
@@ -3,11 +3,14 @@
 require 'rubygems'
 require 'chatterbot/dsl'
 
+# for url encoding
+require 'cgi'
+
 #debug_mode
 verbose
 
 # Ignore our own tweets to prevent a silly cycle of self-replies
-#blacklist "wheresthatsat"
+blacklist "wheresthatsat"
 
 # here we build an index of terms to look for, based on satellite names in our
 # library of supported satellites. Currently hard-coded; should be assembled
@@ -36,12 +39,12 @@ catalog.keys.each do |satellite|
 		# if the tweet is georeferenced, pass its coordinates to gtg as the
 		# observer location and ask for elevation/azimuth attributes
 		# geo documented here: https://dev.twitter.com/docs/api/1/get/statuses/show/%3Aid
-		if tweet[:geo] != nil
-			observer_latitude = tweet[:geo][:coordinates][0]
-			observer_longitude = tweet[:geo][:coordinates][1]
+		#if tweet[:geo] != nil
+		#	observer_latitude = tweet[:geo][:coordinates][0]
+		#	observer_longitude = tweet[:geo][:coordinates][1]
 			# append this junk to the pipe; after the pipe, have another conditional to insert obs angle to reply
-			gtg_cmd += format " --observer %s %s --attributes elevation azimuth", observer_latitude, observer_longitude
-		end
+		#	gtg_cmd += format " --observer %s %s --attributes elevation azimuth", observer_latitude, observer_longitude
+		#end
 				
 		# run gtg to get the coordinates and attribute information.
 		# needs error handling
@@ -64,15 +67,22 @@ catalog.keys.each do |satellite|
 		# we should keep track of the length of the reply, so as not to exceed
 		# the 140 character maximum. Usernames are [mostly] <= 15 characters.
 		# We can round values to a few short places to save space, too.
-		reply_text = format "#USER# When you mentioned %s, it was located above %.4f %s %.4f %s at an altitude of %.2f km.", satellite, latitude.abs, latitude >= 0 ? "N" : "S", longitude.abs, longitude >= 0 ? "E" : "W", altitude
+		map_link = format "http://maps.google.com/?q=%s,%s+(%s)&z=2&output=embed", latitude, longitude, CGI.escape(satellite)
+		reply_text = format "#USER# When you mentioned %s, it was located above %.3f %s %.3f %s at an altitude of %.1f km. %s", satellite, latitude.abs, latitude >= 0 ? "N" : "S", longitude.abs, longitude >= 0 ? "E" : "W", altitude, map_link
+		
+		# links get converted to t.co links
+		# which are a constant length: 20
+		# (well, https links are 21; see https://dev.twitter.com/discussions/4458)
+		#http://t.co/SYGdx0uM
+		# pretty close, as-is; 132 characters with estimated maximum username, link, and coordinates.
 		
 		# append observer-specific attributes to the reply if georeferenced
-		if tweet[:geo] != nil
+		#if tweet[:geo] != nil
 			# should confirm that we got these attributes
-			elevation = info[4].to_f
-			azimuth = info[5].to_f
-			reply_text += format " (El: %.2f, Az: %.2f)", elevation, azimuth
-		end
+		#	elevation = info[4].to_f
+		#	azimuth = info[5].to_f
+		#	reply_text += format " (El: %.2f, Az: %.2f)", elevation, azimuth
+		#end
 		
 		# post the reply
 		reply reply_text, tweet
@@ -81,4 +91,6 @@ catalog.keys.each do |satellite|
 end
 
 # this logs the time of this run so we'll only see new tweets next time
-update_config
+# this does occur automatically at_exit - but is needed explicitly if
+# we plan to run a loop ourself.
+#update_config
