ac4837efc2b4bba43834b2975bf09400890358fe
Jim DeVona <jim.devona@gmail.com>
2012-04-13 08:02:39 -0400

    quoting satellite name in search instead of catalog for simplicity

--- WheresThatSat.rb
+++ WheresThatSat.rb
@@ -7,19 +7,19 @@ require 'chatterbot/dsl'
 verbose
 
 # Ignore our own tweets to prevent a silly cycle of self-replies
-blacklist "wheresthatsat"
+#blacklist "wheresthatsat"
 
 # here we build an index of terms to look for, based on satellite names in our
 # library of supported satellites. Currently hard-coded; should be assembled
 # from what we find available to us provided by the TLE updater script.
 # paths are relative to the directory containing this bot script
-satellites = {
-	'"LANDSAT 5"' => "tle/landsat5.tle",
-	'"LANDSAT 7"' => "tle/landsat7.tle"
+catalog = {
+	"LANDSAT 5" => "tle/landsat5.tle",
+	"LANDSAT 7" => "tle/landsat7.tle"
 }
 
-satellites.keys.each do |sat|
-	search sat do |tweet|
+catalog.keys.each do |satellite|
+	search(format '"%s"', satellite) do |tweet|
 
 		#timestamp = Time.now.getgm.strftime("%Y-%m-%d %H:%M:%S.000000 UTC")		
 		
@@ -30,7 +30,7 @@ satellites.keys.each do |sat|
 		timestamp = created_at_time.to_f
 		
 		# basic command
-		tle_dir = File.expand_path(satellites[sat])
+		tle_dir = File.expand_path(catalog[satellite])
 		gtg_cmd = format '/usr/local/bin/gtg --input "%s" --start "%s" --format csv --attributes altitude', tle_dir, timestamp
 		
 		# if the tweet is georeferenced, pass its coordinates to gtg as the
@@ -64,7 +64,7 @@ satellites.keys.each do |sat|
 		# we should keep track of the length of the reply, so as not to exceed
 		# the 140 character maximum. Usernames are [mostly] <= 15 characters.
 		# We can round values to a few short places to save space, too.
-		reply_text = format "#USER# When you mentioned %s, it was located above %.4f %s %.4f %s at an altitude of %.2f km.", sat[1..-2], latitude.abs, latitude >= 0 ? "N" : "S", longitude.abs, longitude >= 0 ? "E" : "W", altitude
+		reply_text = format "#USER# When you mentioned %s, it was located above %.4f %s %.4f %s at an altitude of %.2f km.", satellite, latitude.abs, latitude >= 0 ? "N" : "S", longitude.abs, longitude >= 0 ? "E" : "W", altitude
 		
 		# append observer-specific attributes to the reply if georeferenced
 		if tweet[:geo] != nil
