9933ceba6ec2c4bd1946b153af8f167f6b260e9a
Jim DeVona <jim.devona@gmail.com>
2012-04-20 15:57:41 -0400

    Added test mode (default for now) to bot; restructured with drafts of most planned reply features in place.

--- WheresThatSat.rb
+++ WheresThatSat.rb
@@ -1,120 +1,193 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
+$testmode = true
+if (ARGV.length == 1 && ARGV[0] == 'furreal')
+	$testmode = false
+else
+	puts "Test mode"
+end
+
 require 'chatterbot/dsl'
-
-# for url encoding
-require 'cgi'
-
+	
 #debug_mode
 #verbose
 
 # Ignore our own tweets to prevent a silly cycle of self-replies
 blacklist "wheresthatsat"
 
-def TheresThatSat(satellite_name, satellite_tle, timestamp, tweet)
+# for url encoding
+require 'cgi'
 
-	# basic command
-	tle_dir = File.expand_path(satellite_tle)
-	gtg_cmd = format '/usr/local/bin/gtg --input "%s" --start "%s" --format csv --attributes altitude', tle_dir, timestamp
-	
-	# if the tweet is georeferenced, pass its coordinates to gtg as the
-	# observer location and ask for elevation/azimuth attributes
-	# geo documented here: https://dev.twitter.com/docs/api/1/get/statuses/show/%3Aid
-	#if tweet[:geo] != nil
-	#	observer_latitude = tweet[:geo][:coordinates][0]
-	#	observer_longitude = tweet[:geo][:coordinates][1]
-		# append this junk to the pipe; after the pipe, have another conditional to insert obs angle to reply
-	#	gtg_cmd += format " --observer %s %s --attributes elevation azimuth", observer_latitude, observer_longitude
-	#end
-			
-	# run gtg to get the coordinates and attribute information.
-	# needs error handling
+def GoGoGTG(gtg_args)
+	gtg_cmd = './gtg ' + gtg_args
 	gtg_pipe = IO.popen(gtg_cmd)
-	gtg_data = gtg_pipe.read.split("\n")
+	gtg_data = gtg_pipe.read
 	gtg_pipe.close
+	return gtg_data.split("\n").collect {|record| record.split(',')}
+end
+
+#
+# satellite_name, display name of satellite
+# tle_path, path to satellite TLE file
+# user_name, input twitter username
+# tweet_id, input tweet_id
+# mention_time - integer unix timestamp of focal time
+# response_time - integer unix timestamp of reply time. Suppress reply time marker if < 0.
+# is_geo, boolean whether observer location is defined (true if yes)
+#
+def TheresThatSat(satellite_name, tle_path, user_name, tweet_id, mention_time, response_time, is_geo, geo_lat, geo_lon)
 	
-	# split the output lines into fields, and find the first [only] record
-	# should confirm that we got the record successfully
-	gtg_data.collect! {|line| line.split(',')}
-	info = gtg_data.detect {|line| line[0] == '0'}
+	url = format 'http://wheresthatsat.com/map.html?sn=%s&un=%s&ut=%s', CGI.escape(satellite_name), CGI.escape(user_name), tweet_id
 	
-	# info pieces that we need for the reply
-	# should confirm that we got all these attributes
-	latitude = info[1].to_f
-	longitude = info[2].to_f
-	altitude = info[3].to_f
+	# trace
+	trace_start_time = mention_time - (4 * 60)
+	trace_end_time = (response_time < 0 ? mention_time : response_time) + (4 * 60)
+	url += format '&t1=%d&t2=%d', trace_start_time, trace_end_time
+	trace_cmd = format '--input "%s" --format csv --start "%d" --end "%d" --interval 1m', tle_path, trace_start_time, trace_end_time
+	trace_data = GoGoGTG(trace_cmd)
+	trace_data.each do |point|
+		url += format '&ll=%.4f,%.4f', point[1], point[2]
+	end
 	
-	# assemble the basic reply
-	# we should keep track of the length of the reply, so as not to exceed
-	# the 140 character maximum. Usernames are [mostly] <= 15 characters.
-	# We can round values to a few short places to save space, too.
-	#map_link = format "http://maps.google.com/?q=%s,%s+(%s)&z=2&output=embed", latitude, longitude, CGI.escape(satellite_name)
-	reply_text = format "#USER# When you mentioned %s, it was located above %.3f %s %.3f %s at an altitude of %.1f km.", satellite_name, latitude.abs, latitude >= 0 ? "N" : "S", longitude.abs, longitude >= 0 ? "E" : "W", altitude
+	# observer
+	if is_geo then url += format '&ol=%.4f,%.4f', geo_lat, geo_lon end
 	
-	# links get converted to t.co links
-	# which are a constant length: 20
-	# (well, https links are 21; see https://dev.twitter.com/discussions/4458)
-	#http://t.co/SYGdx0uM
-	# pretty close, as-is; 132 characters with estimated maximum username, link, and coordinates.
+	# mention
+	mention_cmd = format '--input "%s" --format csv --start "%d" --steps 1 --attributes altitude velocity heading', tle_path, mention_time
+	if is_geo then mention_cmd += format ' --observer %f %f --attributes shadow elevation azimuth solarelev', geo_lat, geo_lon end
+	m = GoGoGTG(mention_cmd)[0]
+	mention_lat = m[1].to_f
+	mention_lon = m[2].to_f
+	url += format '&ml=%.4f,%.4f&ma=%f&ms=%f&mh=%f&mt=%d', m[1], m[2], m[3], m[4], m[5], mention_time
+	if is_geo then url += format'&mi=%d&me=%f&mz=%f&mo=%f', m[6], m[7], m[8], m[9] end
 	
-	# append observer-specific attributes to the reply if georeferenced
-	#if tweet[:geo] != nil
-		# should confirm that we got these attributes
-	#	elevation = info[4].to_f
-	#	azimuth = info[5].to_f
-	#	reply_text += format " (El: %.2f, Az: %.2f)", elevation, azimuth
-	#end
-	
-	# post the reply
-	reply reply_text, tweet
+	# response (none if response_time < 0)
+	if (response_time >= 0)
+		reply_cmd = format '--input "%s" --format csv --start "%d" --steps 1 --attributes altitude velocity heading', tle_path, response_time
+		if is_geo then reply_cmd += format ' --observer %f %f --attributes shadow elevation azimuth solarelev', geo_lat, geo_lon end
+		r = GoGoGTG(reply_cmd)[0]
+		url += format '&rl=%.4f,%.4f&ra=%f&rs=%f&rh=%f&rt=%d', r[1], r[2], r[3], r[4], r[5], response_time
+		if is_geo then url += format '&ri=%d&re=%f&rz=%f&ro=%f', r[6], r[7], r[8], r[9] end
+	end
+
+	# return complete reply text
+	reply_text = format "#USER# When you mentioned %s, it was above %.4f%s %.4f%s. Here's more info: %s",
+			satellite_name, mention_lat.abs, mention_lat >= 0 ? "N" : "S", mention_lon.abs, mention_lon >= 0 ? "E" : "W", url
 
 end
 
-catalog_file = open "config/catalog.txt"
-catalog_lines = catalog_file.read.split("\n")
-catalog_file.close
-catalog = {}
-catalog_lines.each do |catalog_line|
-	name, path = catalog_line.split("\t")
-	catalog[name] = path
+# sets $catalog global variable to hash of satellite names -> TLE file paths
+def LoadSatelliteCatalog(catalog_path='config/catalog.txt')
+	catalog_file = open catalog_path
+	catalog_lines = catalog_file.read.split("\n")
+	catalog_file.close
+	$catalog = {}
+	catalog_lines.each do |catalog_line|
+		name, path = catalog_line.split("\t")
+		$catalog[name] = path
+	end
 end
 
-# # config/sat_searches.txt contains a list of catalog keys (one per line) to search for
-# # - these are the satellites we'll tell people about spontaneously.
-# search_file = open "config/sat_searches.txt"
-# search_satellites = search_file.read.split("\n")
-# search_file.close
-# 
-# search_satellites.each do |satellite|
-# 	search(format '"%s"', satellite) do |tweet|
-# 		
-# 		# skip search results that mention us; we'll handle those as replies
-# 		# - however, if it mentions us because it's retweeting us, we don't
-# 		#   really need to reply to it either. I suppose we should check for RT in replies
-# 		if tweet[:text].match(/@WheresThatSat/i)
-# 			next
-# 		end
-# 		
-# 		# example search created_at timestamp: Fri, 13 Apr 2012 13:06:22 +0000
-# 		tp = tweet[:created_at].split(' ')
-# 		hms = tp[4].split(':')
-# 		timestamp = Time.gm(tp[3], tp[2], tp[1], hms[0], hms[1], hms[2], 0).to_f
-# 		
-# 		TheresThatSat satellite, catalog[satellite], timestamp, tweet
-# 	end
-# end
+# returns integer number of seconds since UNIX epoch
+# example search created_at timestamp: Fri, 13 Apr 2012 13:06:22 +0000
+def ParseSearchTimestamp(created_at)
+	parts = created_at.split(' ')
+	hms = parts[4].split(':')
+	return Time.utc(parts[3], parts[2], parts[1], hms[0], hms[1], hms[2], 0).to_i
+end
 
-replies do |tweet|
-	catalog.keys.each do |satellite|
-		if tweet[:text].match(/\b#{satellite}\b/i)
-			
-			# example reply created_at timestamp: Fri Apr 13 13:06:22 +0000 2012
-			tp = tweet[:created_at].split(' ')
-			hms = tp[3].split(':')
-			timestamp = Time.gm(tp[5], tp[1], tp[2], hms[0], hms[1], hms[2], 0).to_f
-			
-			TheresThatSat satellite, catalog[satellite], timestamp, tweet
+# returns integer number of seconds since UNIX epoch
+# example reply created_at timestamp: Fri Apr 13 13:06:22 +0000 2012
+def ParseReplyTimestamp(created_at)
+	parts = created_at.split(' ')
+	hms = parts[3].split(':')
+	return Time.utc(parts[5], parts[1], parts[2], hms[0], hms[1], hms[2], 0).to_i
+end
+
+def RespondToSearches()
+	# draft code stashed in notes
+	# we won't perform many searches anyway since it really pushes rate limit
+end
+
+def RespondToMentions()
+	replies do |tweet|
+		$catalog.keys.each do |satellite_name|
+			if tweet[:text].match(/\b#{satellite_name}\b/i)
+				
+				# By default, plot ground track from mention time to our reply time.
+				input_timestamp = ParseReplyTimestamp(tweet[:created_at])
+				output_timestamp = Time.now.utc.to_i
+				
+				# Don't respond to this tweet if it's too old (overriding even
+				# our bot interval - we don't want to render any huge ranges
+				# on the map, or try passing them through the URL parameters)
+				if (output_timestamp - input_timestamp > (15 * 60))
+					next
+				end
+				
+				if (tweet[:text].match(/\#time(\d+)/i))
+					# If the user specifies a specific time (which may be long past,
+					# or in the future), plot ground track around that time, and do
+					# not output a separate track point representing our reply time.
+					input_timestamp = $1.to_i
+					output_timestamp = -1
+					if $testmode then puts format "Explicit timestamp: %d", input_timestamp end
+				end
+				
+				# By default, no location associated with input.
+				input_geo = false
+				input_lat = 0
+				input_lon = 0
+				if (tweet[:text].match(/\#latlon([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)/i))
+					# A location was explicitly specified in the tweet.
+					# (Support more flexible and intuitive location specifications
+					# - including geocoding of city names, for example.)
+					input_geo = true
+					input_lat = $1.to_f
+					input_lon = $2.to_f
+					if $testmode then puts format "Explicit geo: %f, %f", input_lat, input_lon end
+				elsif (tweet[:geo] != nil)
+					# A point location is given
+					input_geo = true
+					input_lat = tweet[:geo][:coordinates][0]
+					input_lon = tweet[:geo][:coordinates][1]
+					if $testmode then puts format "Implicit point geo: %f, %f", input_lat, input_lon end
+				elsif (tweet[:place] != nil)
+					# A place location is given
+					input_geo = true
+					bbox = tweet[:place][:bounding_box][:coordinates][0]
+					#bbox[0] # southlat, westlon
+					#bbox[1] # southlat, eastlon
+					#bbox[2] # northlat, eastlon
+					#bbox[3] # northlat, westlon
+					southlat = bbox[0][1]
+					northlat = bbox[2][1]
+					westlon = bbox[0][0]
+					eastlon = bbox[2][0]
+					# bit naive here -- untested on dateline, etc.
+					input_lat = (southlat + northlat) / 2.0
+					input_lon = (westlon + eastlon) / 2.0
+					if $testmode then puts format "Implicit place geo: %f, %f", input_lat, input_lon end
+				end
+	
+				tle_path = $catalog[satellite_name]
+				input_username = from_user(tweet)
+				input_tweetid = tweet[:id]
+								
+				response = TheresThatSat satellite_name, tle_path, input_username, input_tweetid,
+						input_timestamp, output_timestamp, input_geo, input_lat, input_lon
+				if ($testmode)
+					# In test mode, just print the response for inspection.
+					puts response
+				else
+					# Otherwise, post the response in reply to the input Tweet.
+					reply response, tweet
+				end
+			end
 		end
 	end
 end
+
+LoadSatelliteCatalog()
+RespondToSearches()
+RespondToMentions()
