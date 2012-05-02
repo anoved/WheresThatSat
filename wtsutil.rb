006140d557e7fcdeb9da99691ffc4c90870d3d91
Jim DeVona <jim.devona@gmail.com>
2012-05-02 07:30:53 -0400

    New WTSCatalog class takes care of alias resolution, will help keep things tidy.

--- wtsutil.rb
+++ wtsutil.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 
 module WTS
-	
+		
 	def WTS.load_catalog(path='config/catalog.yml')
 		
 		if File.exists?(path)
@@ -29,5 +29,43 @@ module WTS
 		end
 		
 	end
+	
+	class WTSCatalog
 		
+		include Enumerable
+		
+		def initialize(catalogPath='config/catalog.yml')
+			@catalog = WTS.load_catalog(catalogPath)
+		end
+		
+		#
+		# Parameters:
+		#	key, which may be an alias name or a canonical name
+		#
+		# Returns:
+		#	TLE data corresponding to key, or nil if none
+		#
+		def [](key)
+			if @catalog[:alias].has_key?(key)
+				@catalog[:tle][@catalog[:alias][key]]
+			else
+				@catalog[:tle][key]
+			end
+		end
+		
+		#
+		# Results:
+		#	yields each catalog key (alias and canonical)
+		#	
+		def each
+			@catalog[:alias].keys.each do |aliasKey|
+				yield aliasKey
+			end
+			@catalog[:tle].keys.each do |canonicalKey|
+				yield canonicalKey
+			end
+		end
+		
+	end
+	
 end
